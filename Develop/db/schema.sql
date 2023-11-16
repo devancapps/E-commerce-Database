8e53a06912545f296d7191abeb7cee1c327a0a12
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-11-15 22:51:50 -0600

    Resolved foreign key conflicts. Updated routes

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,6 +1,41 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+-- schema.sql
 
--- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+-- Drop existing tables
+SET FOREIGN_KEY_CHECKS = 0;
 
+DROP TABLE IF EXISTS ProductTag;
+DROP TABLE IF EXISTS Tag;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Category;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Create Category table
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
+
+-- Create Product table
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT DEFAULT 10 NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+-- Create Tag table
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255)
+);
+
+-- Create ProductTag table
+CREATE TABLE ProductTag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,3 +1,6 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/connection');
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,12 +8,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
